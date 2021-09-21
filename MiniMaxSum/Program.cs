e431bca46c0a9b79656b5aa135f0398bea4037ee
Anil T <aniltailor@users.noreply.github.com>
2021-09-22 00:00:29 +0100

    Add files via upload

---
+++ MiniMaxSum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace MiniMaxSum
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'miniMaxSum' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void miniMaxSum(List<long> arr)
+        {
+            var minSum = arr.OrderBy( n => n).Take(4).Sum();
+            var maxSum = arr.OrderByDescending(n => n).Take(4).Sum();
+
+            Console.WriteLine($"{minSum} {maxSum}");
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                List<long> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt64(arrTemp)).ToList();
+
+                Result.miniMaxSum(arr);
+            }
+        }
+
+    }
+}
